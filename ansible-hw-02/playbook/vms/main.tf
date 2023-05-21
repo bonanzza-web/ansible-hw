9e44133b4c250bfabc6e72a0da199dbfe38ed98e
Dmitry Ananiev <frukt07@inbox.ru>
2023-05-21 18:03:49 +0300

    add ansible-homework-02

---
+++ ansible-hw-02/playbook/vms/main.tf
@@ -0,0 +1,48 @@
+resource "yandex_vpc_network" "develop" {
+  name = var.vpc_name
+}
+resource "yandex_vpc_subnet" "develop" {
+  name           = var.vpc_name
+  zone           = var.default_zone
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.default_cidr
+}
+
+
+data "yandex_compute_image" "centos" {
+  family = "${var.vm_os}"
+}
+resource "yandex_compute_instance" "platform" {
+  count = 2
+  name        = "vm-${count.index}"
+
+resources {
+    cores         = 2
+    memory        = 1
+    core_fraction = 5
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.centos.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+  }
+
+  metadata = {
+    serial-port-enable = var.vm_metadata.serial-port-enable
+    ssh-keys           = "centos:${local.ssh_key}"
+  }
+}
+
+data "template_file" "config" {
+#  template = file("./config.yml")
+ vars = {
+ ssh_key = local.ssh_key
+ }
+}
